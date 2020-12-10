5dc5cc801019aed2ff4f450f513b6a8a7adfa4f9
Luiz Willner <luizwillner@id.uff.br>
2020-12-10 19:46:13 -0300

    começando a codar

--- programa2.py
+++ programa2.py
@@ -1,1 +1,10 @@
-print('hello world')
+cartela = open('cartelas_doc.txt', 'r')
+
+# maximo = 99
+# globo = [0, 1, 2, 3, 4, 5 ...... 97, 98, 99]
+# todos_os_sorteados = []
+# index = randint(0, maximo)
+# sorteado = globo[index]
+# todos_os_sorteados.append(sorteado)
+# globo.pop(index)
+# maximo = maximo - 1
