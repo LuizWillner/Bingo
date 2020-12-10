ccd367a21722f11f28832626f528ee5977fedd7f
Luiz Willner <luizwillner@id.uff.br>
2020-12-10 20:33:17 -0300

    nada a declarar

--- programa2.py
+++ programa2.py
@@ -1,15 +1,19 @@
+from random import randint
+
+globo = list(range(100))  # criando lista com as 1oo "bolinhas" do bingo (de 0 a 99)
+
+maximo = 99
+todos_os_sorteados = []
+index = randint(0, maximo)
+todos_os_sorteados.append(globo[index])
+globo.pop(index)
+maximo -= 1
+
 cartelas = open('cartelas_doc.txt', 'r')
 lista_cartelas = cartelas.readlines()
 print(lista_cartelas)
+
 for i in range(len(lista_cartelas)):  # da pra substituir depois pelo numero de cartelas de entrada pelo user
-    lista_cartelas[i] = list(map(int, lista_cartelas[i].split()))
+    lista_cartelas[i] = list(map(int, lista_cartelas[i].split()))  # transformando o arquivo das cartelas em uma matriz de inteiros
 
 print(lista_cartelas)
-# maximo = 99
-# globo = [0, 1, 2, 3, 4, 5 ...... 97, 98, 99]
-# todos_os_sorteados = []
-# index = randint(0, maximo)
-# sorteado = globo[index]
-# todos_os_sorteados.append(sorteado)
-# globo.pop(index)
-# maximo = maximo - 1
