3d891a984fe9b1ecd9afb886062508507d35d3b1
Luiz Willner <luizwillner@id.uff.br>
2020-12-10 22:02:41 -0300

    Adicionamos a checagem para verificar se o número sorteado está na cartela

--- cartelas.py
+++ cartelas.py
@@ -1,5 +1,7 @@
-from random import randint #importando a funcao de aleatoriedade
-def selectionsort(lista): #criando o metodo de ordenacao selectionsort
+from random import randint  # importando a funcao de aleatoriedade
+
+
+def selectionsort(lista):  # criando o metodo de ordenacao selectionsort
     i = 0
     while i < len(lista) - 1:
         j = i + 1
@@ -9,27 +11,24 @@ def selectionsort(lista): #criando o metodo de ordenacao selectionsort
             j += 1
         i += 1
     return lista
-ncartelas = int(input()) #quantidade de cartelas a serem geradas
-matrizcartela = [] #lista de todas cartelas
-for x in range(ncartelas): #definindo o numero de cartelas a serem geradas
-    cartela = [] #numeros da cartela
+
+
+ncartelas = int(input())  # quantidade de cartelas a serem geradas
+matrizcartela = []  # lista de todas cartelas
+for x in range(ncartelas):  # definindo o numero de cartelas a serem geradas
+    cartela = []  # numeros da cartela
     while len(cartela) < 10:
-        numero = str(randint(0,99)) #selecionando aleatoriamente os numeros entre 00 e 99 para a cartela
-        if len(numero) == 1: #caso o numero tenha apenas um algarismo adicionar 0 antes
+        numero = str(randint(0, 99))  # selecionando aleatoriamente os numeros entre 00 e 99 para a cartela
+        if len(numero) == 1:  # caso o numero tenha apenas um algarismo adicionar 0 antes
             numero = '0' + numero
         if numero not in cartela:
-            cartela.append(numero) #armazenando os numeros na cartela
-    while cartela in matrizcartela: #evitando cartelas iguais
+            cartela.append(numero)  # armazenando os numeros na cartela
+    while cartela in matrizcartela:  # evitando cartelas iguais
         num = cartela[9]
         while cartela[9] == num:
-            cartela[9] = randint(0,99)
-    cartela = selectionsort(cartela) #aplicando a ordenacao selectionsort nas cartelas criadas
-    matrizcartela.append(cartela)#armazenando as cartelas na lista(matrizcartelas)
-with open('cartelas.txt','w') as arquivo: #abrindo o arquivo
-    for cartela in matrizcartela:#selecionando a cartela para salvar no arquivo em linhas diferentes
+            cartela[9] = randint(0, 99)
+    cartela = selectionsort(cartela)  # aplicando a ordenacao selectionsort nas cartelas criadas
+    matrizcartela.append(cartela)  # armazenando as cartelas na lista(matrizcartelas)
+with open('cartelas.txt', 'w') as arquivo:  # abrindo o arquivo
+    for cartela in matrizcartela:  # selecionando a cartela para salvar no arquivo em linhas diferentes
         arquivo.write(' '.join(cartela) + '\n')
-
-
-
-
-
--- programa2.py
+++ programa2.py
@@ -1,25 +1,35 @@
 from random import randint
 
-cartelas = open('cartelas_doc.txt', 'r')
-    lista_cartelas = cartelas.readlines()
+todas_cartelas = open('cartelas_doc.txt', 'r')
+lista_cartelas = todas_cartelas.readlines()
+matriz_cartelas = [[]] * len(lista_cartelas)  # da pra substituir depois pelo numero de cartelas de entrada pelo user
 
 for i in range(len(lista_cartelas)):  # da pra substituir depois pelo numero de cartelas de entrada pelo user
-        lista_cartelas[i] = list(map(int, lista_cartelas[i].split()))  # transformando o arquivo das cartelas em uma matriz de inteiros
+    matriz_cartelas[i] = list(map(int, lista_cartelas[i].split()))  # transformando o arquivo das cartelas em uma matriz de inteiros
+    matriz_cartelas[i].append([0])
 
+print(lista_cartelas)
+print(matriz_cartelas)
 
 globo = list(range(100))  # criando lista com as 1oo "bolinhas" do bingo (de 0 a 99)
 
 maximo = 99
 todos_os_sorteados = []
+vencedor = False
 
-while maximo >= 0:
+while maximo >= 0 and not vencedor:
     index = randint(0, maximo)
     todos_os_sorteados.append(globo[index])
     globo.pop(index)
     maximo -= 1
 
-    for lista in range(len(lista_cartelas)): # da pra substituir depois pelo numero de cartelas de entrada pelo user
-        
-
-
+    for cartela in matriz_cartelas:
+        if globo[index] in cartela:
+            cartela[-1] += 1
+            if cartela[-1] == 10:
+                vencedor = True  # talvez seja interessante adicionar as cartelas vencedoras numa lista para depois printar qum ganhou
+            elif cartela[-1] == 9:
+                print('A cartela {} está próxima da vitória!'.format(matriz_cartelas.index(cartela)))  # pode ser melhorado
     
+
+
